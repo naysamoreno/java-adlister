efb39168f64dd195e1517a80664243d6cdaa7e41
Naysa Moreno <naysaymoreno@gmail.com>
2021-07-08 14:47:08 -0500

    logout

--- src/main/java/LoginServlet.java
+++ src/main/java/LoginServlet.java
@@ -9,28 +9,35 @@ import java.io.IOException;
 @WebServlet(name = "LoginServlet", urlPatterns = "/login")
 public class LoginServlet extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        if(request.getSession().getAttribute("isAdmin") != null) {
-            response.sendRedirect("/secret-admin-page.jsp");
+        if(request.getSession().getAttribute("user") != null) {
+            response.sendRedirect("/profile");
+            return;
         }
 
-        request.getRequestDispatcher("/login.jsp").forward(request, response);
+        request.getRequestDispatcher("/WEB-INF/login.jsp").forward(request, response);
     }
 
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
         String username = request.getParameter("username");
         String password = request.getParameter("password");
-        boolean isAdmin = username.equals("admin") && password.equals("password");
+//        boolean isAdmin = username.equals("admin") && password.equals("password");
+        boolean isLoggedIn = username.equals("user") && password.equals("password");
 
         HttpSession session = request.getSession();
 
-        if (isAdmin) {
-            session.setAttribute("isAdmin", true);
-            response.sendRedirect("/secret-admin-page");
+//        if (isAdmin) {
+//            session.setAttribute("isAdmin", true);
+//            response.sendRedirect("/secret-admin-page");
+//        } else {
+//            response.sendRedirect("/login");
+//        }
+
+        if (isLoggedIn) {
+            session.setAttribute("user", username);
+            response.sendRedirect("/profile");
         } else {
             response.sendRedirect("/login");
         }
-
-
 
 //       //////////////////////////////////////
 //        // set the session object to a variable based on the HttpSession class
---
+++ src/main/webapp/WEB-INF/user-profile-page.jsp
@@ -0,0 +1,11 @@
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title><%= "some title" %>
+    </title>
+</head>
+<body>
+<h1>Welcome User</h1>
+</body>
+</html>
