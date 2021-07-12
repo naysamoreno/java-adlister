c50142bff7edfa3ed4a9c9e7eb9596ec1f9d9455
Naysa Moreno <naysaymoreno@gmail.com>
2021-07-12 11:17:23 -0500

    user-input-exercise

--- src/main/java/com/codeup/adlister/dao/MySQLAdsDao.java
+++ src/main/java/com/codeup/adlister/dao/MySQLAdsDao.java
@@ -40,9 +40,14 @@ public class MySQLAdsDao implements Ads {
 
     @Override
     public Long insert(Ad ad) {
+        String sql = "INSERT INTO ads(title, description, user_id) VALUES (?, ?, ?);";
         try {
-            Statement stmt = connection.createStatement();
-            stmt.executeUpdate(createInsertQuery(ad), Statement.RETURN_GENERATED_KEYS);
+            PreparedStatement stmt = connection.prepareStatement(sql,Statement.RETURN_GENERATED_KEYS);
+            stmt.setString(1, ad.getTitle());
+            stmt.setString(2, ad.getDescription());
+            stmt.setLong(3, ad.getUserId());
+
+            stmt.executeUpdate();
             ResultSet rs = stmt.getGeneratedKeys();
             rs.next();
             return rs.getLong(1);
@@ -73,5 +78,20 @@ public class MySQLAdsDao implements Ads {
             ads.add(extractAd(rs));
         }
         return ads;
+    }
+
+    @Override
+    public List<Ad> searchByTitle(String query) {
+        String sql = "SELECT * FROM ads WHERE title LIKE ?";
+        String searchTermWithWildCards = "%" +query +"%";
+        PreparedStatement stmt = null;
+        try {
+            stmt = connection.prepareStatement(sql);
+            stmt.setString(1, searchTermWithWildCards);
+            ResultSet rs = stmt.executeQuery(sql);
+            return createAdsFromResults(rs);
+        } catch (SQLException e) {
+            throw new RuntimeException("Error retrieving all ads.", e);
+        }
     }
 }
